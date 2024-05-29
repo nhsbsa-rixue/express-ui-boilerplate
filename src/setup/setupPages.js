7a83db56491d1cbbd0835c7e2f459d912e9b20ea
nhsbsa-rixue <riwang.xue@gmail.com>
2024-05-29 21:59:02 +0100

    combine router

--- src/setup/setupPages.js
+++ src/setup/setupPages.js
@@ -1,7 +1,17 @@
-import router from "../homeRouter/index.js";
+import express from "express";
 
 import * as pages from "../pages/index.js";
 import { validator } from "../validator/index.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const pageList = Object.entries(pages).map(([key, page]) => ({
+    path: page.path || key,
+    heading: page.heading || key,
+  }));
+  return res.render("home", { pageList });
+});
 
 const setupPages = (app) => {
   Object.entries(pages).forEach(([key, page]) => {
